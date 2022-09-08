77cd698855f8a5184746ec141f8afb227ab2cf74
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-09-08 11:21:53 +0200

    faute orthographe

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,6 +2,6 @@ export const aboutData = {
   title: "Who I am",
   description1: "My name's Arthur. I am a full stack developer based in Paris.",
   description2:
-    "Initially graduated from emlyon, I evolved as a consultant and project manager in start up and consulting firm. I decided to start a new professional project and become a web developer by joining Ironhack. At Ironhack, I learned how to use React, Express, MongoDB and much more. I want to work on exciting projects, bring value and imrpove myslef!",
+    "Initially graduated from emlyon, I evolved as a consultant and project manager in start up and consulting firm. I decided to start a new professional project and become a web developer by joining Ironhack. At Ironhack, I learned how to use React, Express, MongoDB and much more. I want to work on exciting projects, bring value and improve myself!",
   image: 2,
 };
